92959c0bf459c6a417908e5d0f0aaf9109f28ba6
sseung <duaqkr2@naver.com>
2024-06-03 16:32:50 +0900

    #4. GoalItem, GoalInput 컴포넌트 분할

---
+++ components/GoalItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const GoalItem = ({ text }) => {
+  return (
+    <View style={styles.goalItem}>
+      <Text style={styles.goalText}>{text}</Text>
+    </View>
+  );
+};
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    color: 'white',
+  },
+
+  goalText: {
+    color: 'white',
+  },
+});
